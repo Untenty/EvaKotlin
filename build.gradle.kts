4449578d371e944817d1e616a248fa06b0d0d465
willson <untenty@gmail.com>
2023-08-23 01:47:54 +0600

    commit 2

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "com.myprojects"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs);
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("net.java.dev.jna:jna:5.13.0")
+    implementation("net.java.dev.jna:jna-platform:5.13.0")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "EvaKotlin"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+buildDir = file("D:\\programming\\kotlin\\EvaKotlin\\out") //my
